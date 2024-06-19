8d29355d54fee99862ee657589e1865dfd1a9169
Eric Jardim <90854156+ericgcjardim@users.noreply.github.com>
2024-06-19 19:45:40 -0300

    .

--- src/index.py
+++ src/index.py
@@ -1,11 +1,11 @@
 from telethon import TelegramClient, events
 import re
 
-api_id = '24176416'
-api_hash = '4493def8358e3ab32cdd7bd0c08406a2'
-phone_number = '5531999592002'
-source_chat = 'PumpLiveKOTH' 
-target_chat = 'paris_trojanbot'  
+api_id = '29979879'
+api_hash = '648942e486bf97a31d9106dbb8cd26f0'
+phone_number = '5531998100509'
+
+target_chat = 'diomedes_trojanbot'
 
 client = TelegramClient('MessageForwarder', api_id, api_hash)
 
@@ -13,49 +13,26 @@ async def main():
     await client.start()
     print("Client Created and Started")
 
-    # Event handler
-    @client.on(events.NewMessage(chats=source_chat))
+    source_chats = ["shmooscasino", "wulfcalls", "degenroshis", "luis100xcalls" , "BossmanCalls", "Cryptocowboyx", "lyxedegen", "evenmoredegen"]  # Add your source chat IDs here
+
+    @client.on(events.NewMessage(chats=source_chats))
     async def handler(event):
         # Message content
         message_content = event.message.message
         print(f"\n\nMENSAGEM NOVA: {message_content}")
-        message_first_social = event.message.buttons[1][0].text
-        print(f"Primeiro social: {message_first_social}")
+        source_chat_id = event.chat_id
+        # Solana address regex
+        solana_address_regex = r'[A-HJ-NP-Za-km-z1-9]{44}'
+        solana_address = re.search(solana_address_regex, message_content)
 
-        whale_regex = r'Whale:\s*(.*?)\s*%'
-        whale_raw = re.search(whale_regex, message_content)
+        if solana_address:
+            token_address = solana_address.group(0)
 
-        dev_regex = r'Dev:\s*(.*?)\s*%'
-        dev_raw = re.search(dev_regex, message_content)
-
-        dev_is_out_regex = r'Dev:\s*(.*?)\s*is Out!'
-        dev_is_out = re.search(dev_is_out_regex, message_content)
-
-        replies_regex = r'Replies:\s*(.*?)\s*📈'
-        replies_raw = re.search(replies_regex, message_content)
-
-        if (whale_raw and (dev_raw or dev_is_out) and replies_raw):
-            whale = float(whale_raw.group(1).split()[1])
-            dev = float(dev_raw.group(1)) if (not dev_is_out) else 0
-            replies = int(replies_raw.group(1))
-
-            combined_percentage = (whale + dev) if whale != dev else whale
-
-            print(f"\nWhale %: {whale}")
-            print(f"Dev %: {dev}")
-            print(f"Combined %: {combined_percentage}")
-            print(f"Total replies: {replies}")
-
-            flag = 6 if (dev == whale or dev == 0) else 12
-            print(f"FLAG: {flag}")
-
-            if(combined_percentage <= flag and replies >= 0 and message_first_social == "Website"):
-                await client.send_message(target_chat, message_content)
-                print(f"TOKEN ENVIADO AO TROJAN!")  
-            else: 
-                print(f"(RUGPULL ALERT) ou (Few replies) ou (Webiste inexistente).")
+            await client.send_message(target_chat, token_address)
+            print(f"Call de {source_chat_id}" )
+            print(f"TOKEN ENVIADO AO TROJAN!")  
         else:
-            return print("\nErro ao encontrar os dados necessários para análise.")
+            print("No Solana address found.")
 
     await client.run_until_disconnected()
 
